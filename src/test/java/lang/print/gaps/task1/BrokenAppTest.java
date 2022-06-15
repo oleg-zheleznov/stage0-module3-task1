9c8e4c2a09936a9f46d2876e7970a88a80680853
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 15:42:53 +0300

    solution

--- src/test/java/lang/print/gaps/task1/BrokenAppTest.java
+++ src/test/java/lang/print/gaps/task1/BrokenAppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BrokenAppTest extends BaseIOTest {
     @Test
     void main(){
-        new BrokenApp().main(null);
+        BrokenApp.main(null);
 
         assertEquals("fixMe\n", updateLineSpliterators(outContent.toString()));
     }
